eb091bab1c57e9dfa92ab7c71d4064d3b1cd2e7f
Thatianne Maruyama <thatianne.maruyama@creathus.org.br>
2023-03-01 09:18:58 -0400

    algumas alterações

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -10,5 +10,4 @@ public class ContaCorrente extends Conta {
 		System.out.println("=== Extrato Conta Corrente ===");
 		super.imprimirInfosComuns();
 	}
-	
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		venilton.setNome("Thatianne");
 		
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+
+		cc.depositar(250);
+		cc.transferir(180, poupanca);
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
